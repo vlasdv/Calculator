d40059e8dccc6a3268d8d23111f72fb712a44236
Dmitrii Vlasov <vlas.dv@gmail.com>
2023-11-08 22:29:29 +0100

    Add function end function to perform operations and index.html

--- static/app.js
+++ static/app.js
@@ -1,15 +1,22 @@
- function add(x, y) {
-  return x + y;
- }
+const operands = [];
+let operator = '';
 
- function subtract(x, y) {
-  return x - y;
- }
+function performOperation(x, y, operation) {
+  return operation(x, y);
+}
 
- function multiply(x, y) {
-  return x * y;  
- }
+function add(x, y) {
+return x + y;
+}
 
- function divide(x, y) {
-  return x / y;
- }
+function subtract(x, y) {
+return x - y;
+}
+
+function multiply(x, y) {
+return x * y;  
+}
+
+function divide(x, y) {
+return x / y;
+}
