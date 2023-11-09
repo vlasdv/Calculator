66f4bc63fe4badf61b74a88a068d4ceb5a20675e
Dmitrii Vlasov <vlas.dv@gmail.com>
2023-11-09 22:15:52 +0100

    Add event listeners for buttons

--- static/app.js
+++ static/app.js
@@ -1,6 +1,29 @@
 const operands = [];
 let operator = '';
 
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((button) => {
+  button.addEventListener('click', e => {
+    // console.log(e);
+    if (e.target.classList.contains('numbers')) {
+      console.log('number');
+    } else if (e.target.classList.contains('operations')) {
+      console.log('operation');
+    } else if (e.target.id === '=') {
+      console.log('=');
+    } else if (e.target.id === ',') {
+      console.log('delete');
+    } else if (e.target.id === 'del') {
+      console.log('delete');
+    } else if (e.target.id === 'ac') {
+      console.log('clear');
+    }
+  });
+});
+
+// addEventListener()
+
 function performOperation(x, y, operation) {
   return operation(x, y);
 }
